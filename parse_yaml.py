d8185c3d8150b389e2710d6f0ef4eeae2ababf59
sinhj <43327964+sinhj@users.noreply.github.com>
2019-07-30 03:02:46 +0800

    Add files via upload

--- parse_yaml.py
+++ parse_yaml.py
@@ -3,10 +3,14 @@ import os, sys
 # reload(sys)
 # sys.setdefaultencoding('utf8')
 
-from yaml import load
+from yaml import safe_load
 # python -m pip install pyyaml
 
-conf = load(open("ftp_cfg.yml", 'rb').read())
+conf = safe_load(open("ftp_cfg.yml", 'rb').read())
+
+print conf.get("subj")
+
+print conf.get("languages")[0]
 
 print conf.get("ftp_0")['passwd']
-print conf.get("ftp_1")
+print conf.get("ftp_1")['port']
